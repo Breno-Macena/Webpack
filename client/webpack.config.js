fd1f0e2d81eb4fc69341670f10a36bc693026526
Breno Macena Pereira de Souza <breno@i3t.com.br>
2021-02-24 21:01:59 -0300

    .

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,8 +2,8 @@ const path = require("path");
 const babiliPlugin = require("babili-webpack-plugin");
 
 let plugins = [];
-if (process.env.NODE_ENV == 'production'){
-    plugins.push(new babiliPlugin());
+if (process.env.NODE_ENV == "production") {
+  plugins.push(new babiliPlugin());
 }
 
 module.exports = {
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-    publicPath: 'dist'
+    publicPath: "dist",
   },
   module: {
     rules: [
@@ -23,10 +23,26 @@ module.exports = {
         },
       },
       {
-        text: /\.css$/,
-        loader: 'style-loader!css-loader'
-      }
+        test: /\.css$/,
+        loader: "style-loader!css-loader",
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff",
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream",
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml",
+      },
     ],
   },
-  plugins
+  plugins,
 };
